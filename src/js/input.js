9956a0e3706e529a063ba084046415fd3a29edff
Likeur <jacksimba28@gmail.com>
2024-01-07 22:23:52 +0200

    working on herosection

--- src/js/input.js
+++ src/js/input.js
@@ -1,31 +1,68 @@
+import gsap from "gsap"
 
-//   document.addEventListener( 'DOMContentLoaded', function() {
-//     var splide = new Splide( '.splide',{
-//         perPage: 4,
-//         type    : 'loop',
-//         autoplay: 'play',
-//         perMove: 1,
-//         arrows: false,
-//         pagination: false,
-//         interval : 2000,
-//         breakpoints : {
-//             400 : {
-//                 perPage: 1,
-//             }
-//         }
-//     } );
-//     splide.mount();
-//   } );
+const btnMenuOpen = document.querySelector('#menu-open')
+const btnMenuClose = document.querySelector('#menu-close')
+const menu = document.querySelector('#navigation')
 
-const btnMenu = document.querySelector('#menubtn')
-const barUn = document.querySelector('#bar-1')
-const barDeux = document.querySelector('#bar-2')
-const menu = document.querySelector('#menu')
+const liens = document.querySelectorAll('.link');
 
-btnMenu.addEventListener('click', () => {
-    menu.classList.toggle('scale-0')
-    barUn.classList.toggle('rotate-45')
-    barUn.classList.toggle('absolute')
-    barDeux.classList.toggle('mt-2')
-    barDeux.classList.toggle('-rotate-45')
+let mouseX
+let mouseY
+
+window.addEventListener('mousemove',(e) =>{
+    mouseX = e.clientX
+    mouseY = e.clientY
+
+    gsap.to('#mouse_follow',{
+        x: mouseX,
+        y: mouseY,
+        duration:0.5
+    })
+})
+
+liens.forEach((lien) => {
+    lien.addEventListener('mouseenter', (e) => {
+      const lienSurvole = e.target;
+      const autresLiens = [...liens].filter((l) => l !== lienSurvole);
+      gsap.to(autresLiens, { color: 'gray' });
+    });
+  
+    lien.addEventListener('mouseleave', () => {
+      gsap.to(liens, { color: 'black' });
+    });
+  });
+
+gsap.set(menu,{
+    x:-1000
+})
+gsap.set('#wrapper',{
+    opacity:0,
+    zIndex:0
+})
+
+const tl = gsap.timeline()
+
+btnMenuOpen.addEventListener('click', () => {
+    
+    gsap.to("#wrapper", {
+        opacity:1,
+        zIndex: 40,
+        duration:0.3
+    })
+    gsap.to(menu, {
+        x:0,
+        duration:0.5,
+        ease:'ease-out'
+    })
+})
+btnMenuClose.addEventListener('click', () => {
+    tl.to(menu, {
+        x:-1000,
+        duration:0.5,
+        ease:'ease-out'
+    }).to("#wrapper", {
+        opacity:0,
+        zIndex: 0,
+        duration:0.3
+    })
 })
